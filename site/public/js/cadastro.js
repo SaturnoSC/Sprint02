0ae8ee6d57c316f103aa1816fac53eef9b9e16a9
Daniel-Claytonn <daniel.almeida@sptech.school>
2022-12-01 18:20:38 -0300

    Finalizada a tela de cadastro, falta o focus do Daniel

---
+++ site/public/js/cadastro.js
@@ -0,0 +1,8 @@
+function mostrarCadPlanta() {
+    document.getElementById('cad_plantacao_div').style.bottom = "0"
+}
+
+function fecharCadPlanta() {
+    document.getElementById('cad_plantacao_div').style.bottom = "-501px"
+
+}
