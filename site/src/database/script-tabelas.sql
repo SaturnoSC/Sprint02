32824146f2c353418314fb33ee76f6ca55998d2f
Daniel-Claytonn <daniel.almeida@sptech.school>
2022-12-02 00:14:38 -0300

    Novo script pronto, alterei algumas coisas minimas nos nomes das colunas e tabelas

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,107 +1,107 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE Capulus;
 
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
+USE Capulus;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+create table Usuario (
+    idUsuario int primary key auto_increment,
+    Nome varchar(45),
+    Email varchar(45),
+    senha varchar(45)
 );
 
-
-
-create table teste (
-	nome varchar(45)
+create table Plantacao (
+    idPlantacao int primary key auto_increment,
+    Nome varchar(45),
+    Metragem int,
+    Tipo varchar(45),
+    CNPJ char(14),
+    fkUsuario int,
+    foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
-
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table Setor (
+    idSetor int primary key,
+    fkPlantacao int,
+    foreign key (fkPlantacao) references Plantacao(idPlantacao),
+    Nome varchar (45)
 );
 
-create table aquario (	
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
+create table Medida (
+    idMedida int primary key auto_increment,
+    DHT11_Temperatura decimal(4, 2),
+    DHT11_Umidade decimal(4, 2),
+    DataHora datetime,
+    fkSetor int,
+    foreign key (fkSetor) references Setor(idSetor),
+    fkSetor_Plantacao int,
+    foreign key (fkSetor_Plantacao) references setor(fkPlantacao)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
-
-/*
-comandos para criar usuário em banco de dados azure, sqlserver,
-com permissão de insert + update + delete + select
-*/
-
-CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
-WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
-DEFAULT_SCHEMA = dbo;
-
-EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
-
-EXEC sys.sp_addrolemember @rolename = N'db_datareader',
-@membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+-- INSERT INTO Usuario
+-- VALUES (
+--         null,
+--         'Julio',
+--         'Silva',
+--         'juliosilva@gamil.com',
+--         '11-95850-1205',
+--         '35645801280',
+--         '9444518000104',
+--         '1989-05-10'
+--     ),
+--     (
+--         null,
+--         'Rosana',
+--         'Garcia',
+--         'rosana.g.arcia@outlook.com',
+--        '11-97789-0626',
+--        '45805498889',
+--         '14240469000102',
+--         '1984-11-25'
+--     );
+-- INSERT INTO Plantacao
+-- VALUES (
+-- 			1, 1, 'Recanto do Aroma', 5000, 'Arábica'
+-- 		  ),
+--        (
+-- 			2, 2, 'Plantação Café Zé', 1000, 'Robusta'
+--        );
+-- INSERT INTO Setor
+-- VALUES (
+-- 			1, 'Setor1', 1
+-- 	      ),
+--        (
+-- 			2, 'Quadra14', 2
+-- 		   );
+-- INSERT INTO Sensor
+-- VALUES (
+--         1,
+--         'A-01',
+--         '19',
+--         '55',
+--         '2022-10-19 09:50:17',
+--         1,
+--         1
+--     ),
+--     (
+--         2,
+--         'A-02',
+--         '23',
+--         '37',
+--         '2022-10-19 10:18:39',
+--         1,
+--        1
+--     );
+SELECT *
+FROM Usuario;
+SELECT *
+FROM Plantacao;
+SELECT *
+FROM Setor;
+SELECT *
+FROM Medida;
+SELECT *
+FROM Usuario
+    JOIN Plantacao ON idUsuario = fkUsuario;
+select * from plantacao join setor on idPlantacao = fkPlantacao;
+select * from setor join medida on idSetor = fkSetor;
+select * from usuario join Plantacao on idUsuario = fkUsuario join setor on idPlantacao = fkPlantacao join medida on idSetor = fkSetor;
