b249a404f5f1447f99a22095db440325a711af9a
AugustoCesar15 <augusto.costa@sptech.school>
2022-12-02 10:58:21 -0300

    Rodando banco de dados na nuvem

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "svr-projeto-saturnosc.database.windows.net",
+    database: "Satruno SC",
+    user: "admin-projeto-teste",
+    password: "#Gfgrupo2",
     pool: {
         max: 10,
         min: 0,
