7b25d7a4f615ba5d742bf1c8480015a8f8131fec
Daniel-Claytonn <daniel.almeida@sptech.school>
2022-12-05 10:40:38 -0300

    Mudando repositório do arduino, ligando dashboard com a api 100%, só falta alertas

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -12,7 +12,7 @@ function buscarUltimasMedidas(idSetor, limite_linhas) {
                         FORMAT(datahora, 'HH:mm:ss') as momento_grafico
                     from medida
                     where fkSetor = ${idSetor}
-                    order by id desc`;
+                    order by idMedida desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
         dht11_temperatura as temperatura, 
@@ -42,7 +42,7 @@ function buscarMedidasEmTempoReal(idSetor) {
                         CONVERT(varchar, datahora, 108) as momento_grafico, 
                         fkSetor 
                         from medida where fkSetor = ${idSetor} 
-                    order by id desc`;
+                    order by idMedida desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
@@ -51,7 +51,7 @@ function buscarMedidasEmTempoReal(idSetor) {
                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+                    order by idMedida desc limit 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
