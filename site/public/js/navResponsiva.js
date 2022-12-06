51a72a89f030dfa6a2a30e2c483fd9a7d08df663
Daniel-Claytonn <daniel.almeida@sptech.school>
2022-12-06 15:13:08 -0300

    Responsivo para tablet o index e o Simulador

---
+++ site/public/js/navResponsiva.js
@@ -0,0 +1,7 @@
+function navResponsiva() {
+    document.getElementById('conteudo-segunda-nav').style.top = "0"
+}
+
+function fecharnavResponsiva() {
+    document.getElementById('conteudo-segunda-nav').style.top = "-105vh"
+}
